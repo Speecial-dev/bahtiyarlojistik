5e59b8d0d78b6c20b7c604998221a632f3c0983c
Speecial-dev <speecial.alp1@gmail.com>
2024-09-10 22:57:32 +0300

    ses

--- server.js
+++ server.js
@@ -4,14 +4,20 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Listen on `port` and 0.0.0.0
-app.listen(port, "0.0.0.0", function () {
-    // ...
-});
-// Statik dosyalar için dizini ayarlayın
-app.use(express.static(path.join(__dirname, '/')));
+// Statik dosyalar için 'public' dizinini ayarlayın
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Tüm isteklerde ana sayfayı döndür
+// Anasayfayı döndür
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Diğer tüm isteklerde de anasayfayı döndür (SPA kullanıyorsanız)
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Sunucuyu başlat ve dinlemeye başla
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
