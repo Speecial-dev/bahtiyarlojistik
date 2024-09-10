3d267727e497f82580c63b0ec86330625d4cf41d
Speecial-dev <speecial.alp1@gmail.com>
2024-09-10 22:48:09 +0300

    forth

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
+// Listen on `port` and 0.0.0.0
+app.listen(port, "0.0.0.0", function () {
+    // ...
+});
 // Statik dosyalar için dizini ayarlayın
 app.use(express.static(path.join(__dirname, '/')));
 
 // Tüm isteklerde ana sayfayı döndür
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
 });
