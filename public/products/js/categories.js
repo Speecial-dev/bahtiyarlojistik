bd34fe34923a34fd6ba0dc7034117496864b4ec3
Speecial-dev <speecial.alp1@gmail.com>
2024-09-20 23:55:35 +0300

    herewegoAgain

---
+++ public/products/js/categories.js
@@ -0,0 +1,13 @@
+// js/categories.js
+
+var categories = {
+    "T-shirt": {
+        "name": "T-shirt",
+        "image": "images/categories/tshirtCategories.jpg"
+    },
+    "genc-giyim": {
+        "name": "Sweatshirt",
+        "image": "images/categories/ts.jpg"
+    }
+    // Daha fazla kategori ekleyebilirsiniz
+};
---
+++ public/products/js/products.js
@@ -0,0 +1,56 @@
+var products = [
+    {
+        "id": "1",
+        "name": "T-shirt",
+        "price": "₺100.00",
+        "images": ["images/products/Polo Yaka T-shirt Model1/format_webp.jpg", "images/products/Polo Yaka T-shirt Model1/1_org_zoom.jpg", "images/products/Polo Yaka T-shirt Model1/maviSpreyTshirt.jpg", "images/products/Polo Yaka T-shirt Model1/siyahTshirt.jpg", "images/products/Polo Yaka T-shirt Model1/maviSpreyTshirt.jpg", "images/products/Polo Yaka T-shirt Model1/kırmızıTshirt.jpg"],
+        "description": "Bebek elbisesi açıklaması burada yer alır.",
+        "sizes": ["S", "M", "L"],
+        "category": "T-shirt"
+    },
+    {
+        "id": "2",
+        "name": "Genç Tişört 1",
+        "price": "₺150.00",
+        "images": ["images/products/urun2.jpg", "images/products/urun2-2.jpg", "images/products/urun2-3.jpg"],
+        "description": "Genç tişört açıklaması burada yer alır.",
+        "sizes": ["S", "M", "L", "XL"],
+        "category": "genc-giyim"
+    },
+    {
+        "id": "4",
+        "name": "Bebek Elbisesi 3",
+        "price": "₺120.00",
+        "images": ["images/products/urun3.jpg", "images/products/urun3-2.jpg", "images/products/urun3-3.jpg"],
+        "description": "Bebek elbisesi açıklaması burada yer alır.",
+        "sizes": ["S", "M", "L"],
+        "category": "T-shirt"
+    },
+    {
+        "id": "5",
+        "name": "Bebek Elbisesi 4",
+        "price": "₺120.00",
+        "images": ["images/products/urun3.jpg", "images/products/urun3-2.jpg", "images/products/urun3-3.jpg"],
+        "description": "Bebek elbisesi açıklaması burada yer alır.",
+        "sizes": ["S", "M", "L"],
+        "category": "T-shirt"
+    },
+    {
+        "id": "6",
+        "name": "Bebek Elbisesi 5",
+        "price": "₺120.00",
+        "images": ["images/products/urun3.jpg", "images/products/urun3-2.jpg", "images/products/urun3-3.jpg"],
+        "description": "Bebek elbisesi açıklaması burada yer alır.",
+        "sizes": ["S", "M", "L"],
+        "category": "T-shirt"
+    }, {
+        "id": "7",
+        "name": "Bebek Elbisesi 6",
+        "price": "₺120.00",
+        "images": ["images/products/urun3.jpg", "images/products/urun3-2.jpg", "images/products/urun3-3.jpg"],
+        "description": "Bebek elbisesi açıklaması burada yer alır.",
+        "sizes": ["S", "M", "L"],
+        "category": "T-shirt"
+    }
+    // Daha fazla ürün ekleyebilirsiniz
+];
